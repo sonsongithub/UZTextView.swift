de460ef4031e56292fae9b1d4fcccba209703f94
sonson <yoshida.yuichi@gmail.com>
2017-03-24 18:01:19 +0900

    Added rendering basic code.

--- UZTextView/UZTextView.swift
+++ UZTextView/UZTextView.swift
@@ -7,8 +7,65 @@
 //
 
 import UIKit
+import CoreText
+
+extension NSAttributedString {
+    var fullRange: CFRange {
+        return CFRange(location: 0, length: self.length)
+    }
+}
+
+extension CFRange {
+    static var zero: CFRange {
+        return CFRange(location: 0, length: 0)
+    }
+}
 
 public class UZTextView: UIView {
-
-
+    var ctframe: CTFrame?
+    var ctframeSetter: CTFramesetter?
+    var contentSize: CGSize = CGSize.zero
+    
+    var contentInset: UIEdgeInsets = UIEdgeInsets.zero
+    
+    public var attributedString: NSAttributedString = NSAttributedString(string: "") {
+        didSet {
+            updateLayout()
+        }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func updateLayout() {
+        let horizontalMargin = contentInset.left + contentInset.right
+        contentSize = CGSize(width: self.frame.size.width - horizontalMargin, height: CGFloat.greatestFiniteMagnitude)
+        let frameSetter = CTFramesetterCreateWithAttributedString(attributedString)
+        let frameSize = CTFramesetterSuggestFrameSizeWithConstraints(frameSetter, attributedString.fullRange, nil, contentSize, nil)
+        contentSize = frameSize
+        let contentRect = CGRect(origin: CGPoint.zero, size: frameSize)
+        let path = CGPath(rect: contentRect, transform: nil)
+        ctframe = CTFramesetterCreateFrame(frameSetter, CFRange.zero, path, nil)
+        ctframeSetter = frameSetter
+        
+        setNeedsDisplay()
+    }
+    
+    override public func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        guard let ctframe = ctframe else { return }
+        
+        context.translateBy(x: margin.left, y: margin.top)
+        context.saveGState()
+        context.translateBy(x: 0, y: contentSize.height)
+        context.scaleBy(x: 1, y: -1)
+        context.textMatrix = CGAffineTransform.identity
+        CTFrameDraw(ctframe, context)
+        context.restoreGState()
+    }
 }
--- UZTextViewSample/ViewController.swift
+++ UZTextViewSample/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import UZTextView
 
 class ViewController: UIViewController {
+    @IBOutlet var textView: UZTextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        do {
+            let data = try Data(contentsOf: Bundle.main.url(forResource: "data", withExtension: "html")!)
+            let options: [String: Any] = [
+                NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue
+            ]
+            let attr = try NSMutableAttributedString(data: data, options: options, documentAttributes: nil)
+            attr.addAttribute(NSFontAttributeName,
+                              value: UIFont.systemFont(ofSize: 20),
+                              range: NSRange(location: 0, length: attr.length))
+            textView.attributedString = attr
+            
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {

4c5c65a91b59e289acb9222e43d7d6129b959b7b
sonson <yoshida.yuichi@gmail.com>
2017-03-24 13:51:51 +0900

    First commit.

---
+++ UZTextView/UZTextView.h
@@ -0,0 +1,19 @@
+//
+//  UZTextView.h
+//  UZTextView
+//
+//  Created by sonson on 2017/03/24.
+//  Copyright © 2017年 sonson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UZTextView.
+FOUNDATION_EXPORT double UZTextViewVersionNumber;
+
+//! Project version string for UZTextView.
+FOUNDATION_EXPORT const unsigned char UZTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UZTextView/PublicHeader.h>
+
+
---
+++ UZTextView/UZTextView.swift
@@ -0,0 +1,14 @@
+//
+//  UZTextView.swift
+//  UZTextView
+//
+//  Created by sonson on 2017/03/24.
+//  Copyright © 2017年 sonson. All rights reserved.
+//
+
+import UIKit
+
+public class UZTextView: UIView {
+
+
+}

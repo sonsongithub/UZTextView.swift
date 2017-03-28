de7e3d3c03f34f3ec2a7d812c98504d5de855746
sonson <yoshida.yuichi@gmail.com>
2017-03-28 18:39:21 +0900

    Moved files.

---
+++ UZTextView/UZLoupe.swift
@@ -0,0 +1,12 @@
+//
+//  UZLoupe.swift
+//  UZTextView
+//
+//  Created by sonson on 2017/03/28.
+//  Copyright © 2017年 sonson. All rights reserved.
+//
+
+import UIKit
+
+class UZLoupe: UIView {
+}
